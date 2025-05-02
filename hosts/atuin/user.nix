d1dbcee1307e45682db501f1cc7aeb3fb61164df
Vojtěch Balák <vojtech@balak.me>
2025-05-02 18:26:45 +0200

    Add some packages to atuin

--- hosts/atuin/user.nix
+++ hosts/atuin/user.nix
@@ -6,10 +6,21 @@
   };
   home.packages = with pkgs; [
     awscli2
+    bat
+    direnv
+    fastfetch
+    fd
+    fzf
+    git-crypt
+    htop
     just
     k9s
+    neovim
     nix-search
     nix-tree
+    ripgrep
+    tig
+    wireguard-tools
   ];
   programs.zsh.initContent = ''
     source /home/vojta/.config/op/plugins.sh
@@ -20,10 +31,8 @@
 
     # Disable modules that I haven't tested on Fedora
     packages.enable = false;
-    music.enable = false;
     fonts.enable = false;
     gnome.enable = false;
-    virt-manager.enable = false;
     gpg.enable = false;
     flatpak.enable = false;
   };
