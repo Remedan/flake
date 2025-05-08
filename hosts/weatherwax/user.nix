7d688f7f95496672039708d9ce83b33a5e11a79b
Vojtěch Balák <vojtech@balak.me>
2025-05-08 19:17:11 +0200

    Enable Rust on weatherwax

--- hosts/weatherwax/user.nix
+++ hosts/weatherwax/user.nix
@@ -8,6 +8,7 @@
     music.libraryLocation = "~/Network/Media/Audio/Music";
     dev = {
       python.enable = true;
+      rust.enable = true;
       nodejs.enable = true;
       godot.enable = true;
     };
