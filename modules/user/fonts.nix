eb23ef1c035530c9d4353845fdff8513828fb257
Vojtěch Balák <vojtech@balak.me>
2025-05-15 18:59:02 +0200

    Remove font configuration that is no longer needed

--- modules/user/fonts.nix
+++ modules/user/fonts.nix
@@ -32,36 +32,5 @@ in
       source-sans
       source-serif
     ];
-    xdg.configFile."fontconfig/conf.d/20-no-embedded-bitmap.conf".text = ''
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
-      <fontconfig>
-        <match target="font">
-          <edit name="embeddedbitmap" mode="assign">
-            <bool>false</bool>
-          </edit>
-        </match>
-      </fontconfig>
-    '';
-    xdg.configFile."fontconfig/conf.d/55-emoji-prepend.conf".text = ''
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
-      <fontconfig>
-        <match>
-          <test name="family"><string>sans-serif</string></test>
-          <edit mode="prepend" name="family" binding="weak"><string>Noto Color Emoji</string></edit>
-        </match>
-
-        <match>
-          <test name="family"><string>serif</string></test>
-          <edit mode="prepend" name="family" binding="weak"><string>Noto Color Emoji</string></edit>
-        </match>
-
-        <match>
-          <test name="family"><string>Apple Color Emoji</string></test>
-          <edit mode="prepend" name="family" binding="weak"><string>Noto Color Emoji</string></edit>
-        </match>
-      </fontconfig>
-    '';
   };
 }
