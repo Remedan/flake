d24187ee28860d108662683b553d948b2d4fbf4d
Vojtěch Balák <vojtech@balak.me>
2025-05-21 11:16:33 +0200

    Don't show trailing whitespace in vterm

--- modules/user/emacs/doom/config.el
+++ modules/user/emacs/doom/config.el
@@ -78,7 +78,11 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
+# Show trailing whitespace unless in vterm
 (setq-default show-trailing-whitespace t)
+(add-hook
+ 'vterm-mode-hook
+ (lambda() (setq show-trailing-whitespace nil)))
 
 (use-package! nix-mode
   :mode "\\.nix\\'")
