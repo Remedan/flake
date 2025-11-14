b0cbbe903feaec6dccbf7c177b17c53aeffaa223
Vojtěch Balák <vojtech@balak.me>
2025-11-14 21:16:55 +0100

    Revert "Switch Emacs to the default package"
    This reverts commit a73f17635e61f117e1418503ced426966c5c193a.

--- modules/user/emacs/default.nix
+++ modules/user/emacs/default.nix
@@ -32,6 +32,7 @@ in
     };
     programs.emacs = {
       enable = true;
+      package = pkgs.emacs-pgtk; # The pgtk version has native Wayland support
       extraPackages = epkgs: with epkgs; [
         vterm # This way we don't have to build vterm's compiled component
       ];
@@ -44,5 +45,30 @@ in
     systemd.user.services.emacs.Service.Environment = mkIf
       (cfg.service && config.user-modules.kitty.enable)
       [ "TERMINFO=${pkgs.kitty}/lib/kitty/terminfo" ];
+
+    # Since the Emacs pgtk packages sets the wmclass to "emacs" (with lower-case e) we need to create
+    # a new .desktop file that has a matching StartupWMCLass for Gnome to recognize the window.
+    # This also enables us to add the Calculator action.
+    # Mostly taken from: https://github.com/nix-community/home-manager/blob/master/modules/services/emacs.nix#L19
+    xdg.desktopEntries = {
+      emacsclient = {
+        name = "Emacs Client";
+        exec = "${config.programs.emacs.package}/bin/emacsclient -c %F";
+        icon = "emacs";
+        comment = "Edit text";
+        genericName = "Text Editor";
+        mimeType = [ "text/english" "text/plain" "text/x-makefile" "text/x-c++hdr" "text/x-c++src" "text/x-chdr" "text/x-csrc" "text/x-java" "text/x-moc" "text/x-pascal" "text/x-tcl" "text/x-tex" "application/x-shellscript" "text/x-c" "text/x-c++" ];
+        categories = [ "Development" "TextEditor" ];
+        actions = {
+          "Calculator" = {
+            exec = "${config.programs.emacs.package}/bin/emacsclient -ce \"(full-calc)\"";
+          };
+        };
+        settings = {
+          Keywords = "Text;Editor";
+          StartupWMClass = "emacs";
+        };
+      };
+    };
   };
 }
