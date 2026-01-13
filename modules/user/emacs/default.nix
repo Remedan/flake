2ee68a6c906aee54aa887140806c94d5f9cb24f6
Vojtěch Balák <vojtech@balak.me>
2026-01-13 10:57:53 +0100

    Enable integration between vterm and zsh

--- modules/user/emacs/default.nix
+++ modules/user/emacs/default.nix
@@ -44,5 +44,28 @@ in
     systemd.user.services.emacs.Service.Environment = mkIf
       (cfg.service && config.user-modules.kitty.enable)
       [ "TERMINFO=${pkgs.kitty}/lib/kitty/terminfo" ];
+    # Integration between vterm and zsh
+    # https://github.com/akermu/emacs-libvterm/blob/master/README.md#shell-side-configuration
+    programs.zsh.initContent = mkOrder 1200 ''
+      vterm_printf() {
+          if [ -n "$TMUX" ] \
+              && { [ "''${TERM%%-*}" = "tmux" ] \
+                  || [ "''${TERM%%-*}" = "screen" ]; }; then
+              # Tell tmux to pass the escape sequences through
+              printf "\ePtmux;\e\e]%s\007\e\\" "$1"
+          elif [ "''${TERM%%-*}" = "screen" ]; then
+              # GNU screen (screen, screen-256color, screen-256color-bce)
+              printf "\eP\e]%s\007\e\\" "$1"
+          else
+              printf "\e]%s\e\\" "$1"
+          fi
+      }
+
+      vterm_prompt_end() {
+          vterm_printf "51;A$(whoami)@$(hostname):$(pwd)"
+      }
+      setopt PROMPT_SUBST
+      PROMPT=$PROMPT'%{$(vterm_prompt_end)%}'
+    '';
   };
 }
