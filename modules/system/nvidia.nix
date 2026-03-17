55fa5aa91b9063a96be72987826475cacefba699
Vojtěch Balák <vojtech@balak.me>
2026-03-17 21:33:37 +0100

    Explicitly enable modesetting and power management
    for Nvidia

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -20,6 +20,8 @@ in
     services.xserver.videoDrivers = [ "nvidia" ];
     hardware.nvidia = {
       open = true;
+      modesetting.enable = true;
+      powerManagement.enable = true;
       package = config.boot.kernelPackages.nvidiaPackages.${cfg.driverVersion};
     };
   };
