2fbcf601bb41228b99418bcb107d51a99efd24b9
Vojtěch Balák <vojtech@balak.me>
2026-03-17 21:33:19 +0100

    Remove verbose comments from the Nvidia module

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -13,22 +13,13 @@ in
   };
 
   config = mkIf cfg.enable {
-    # Enable graphics driver
     hardware.graphics = {
       enable = true;
       enable32Bit = true;
     };
-
-    # Load nvidia driver for Xorg and Wayland
     services.xserver.videoDrivers = [ "nvidia" ];
-
     hardware.nvidia = {
-      # Use the NVidia open source kernel module (not to be confused with the
-      # independent third-party "nouveau" open source driver).
       open = true;
-
-      # Select the driver version
-      # https://wiki.nixos.org/wiki/Nvidia#Determining_the_correct_driver_version
       package = config.boot.kernelPackages.nvidiaPackages.${cfg.driverVersion};
     };
   };
