5c5a167fd7cab53b2c015e4b78c4ca9e51bcc2cb
Vojtěch Balák <vojtech@balak.me>
2026-09-04 21:24:40 +0200

    Pin picard and trezorctl

--- flake.nix
+++ flake.nix
@@ -28,6 +28,8 @@
       };
       # Overlay for packages that are broken in unstable
       stablePkgs = final: prev: inputs.nixpkgs.lib.getAttrs [
+        "picard"
+        "trezorctl"
       ] inputs.nixpkgs-stable.legacyPackages.${system};
       overlays = [
         inputs.nix-vscode-extensions.overlays.default
