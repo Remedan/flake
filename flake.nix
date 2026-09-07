d3fc350aeae9c7012a7d03d075cd350fe1c8a923
Vojtěch Balák <vojtech@balak.me>
2026-09-07 10:59:59 +0200

    Pin qmk to stable

--- flake.nix
+++ flake.nix
@@ -29,6 +29,7 @@
       # Overlay for packages that are broken in unstable
       stablePkgs = final: prev: inputs.nixpkgs.lib.getAttrs [
         "picard"
+        "qmk"
         "trezorctl"
       ] inputs.nixpkgs-stable.legacyPackages.${system};
       overlays = [
