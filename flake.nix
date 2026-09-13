f5de8805f77a5888229fc9fccac38b320f384617
Vojtěch Balák <vojtech@balak.me>
2026-09-13 20:36:40 +0200

    Unpin packages

--- flake.nix
+++ flake.nix
@@ -29,9 +29,6 @@
       # Overlay for packages that are broken in unstable
       stablePkgs = final: prev: inputs.nixpkgs.lib.getAttrs
         [
-          "picard"
-          "qmk"
-          "trezorctl"
         ]
         inputs.nixpkgs-stable.legacyPackages.${system};
       overlays = [
