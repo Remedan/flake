dcb9f43bb4c7b4a3d82fcaf7eab815f9b51df9cd
Vojtěch Balák <vojtech@balak.me>
2026-09-17 13:59:36 +0200

    Update renamed option

--- modules/system/base.nix
+++ modules/system/base.nix
@@ -216,7 +216,7 @@ in
 
       # Enable Logitech devices support and Solaar
       hardware.logitech.wireless.enable = true;
-      hardware.logitech.wireless.enableGraphical = true;
+      programs.solaar.enable = true;
 
       # Udev
       services.udev.packages = with pkgs; [
