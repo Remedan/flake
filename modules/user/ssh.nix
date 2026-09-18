aa097b61de10f783ed44ac44ab09ec2b8e01b5ea
Vojtěch Balák <vojtech@balak.me>
2026-09-18 11:18:32 +0200

    Pin twoflower ssh keys

--- modules/user/ssh.nix
+++ modules/user/ssh.nix
@@ -7,15 +7,28 @@ in
     enable = lib.mkEnableOption "SSH";
   };
   config = lib.mkIf cfg.enable {
+    # We have to pin specific keys for specific hosts to avoid too many authentication failures
     home.file.".ssh/codeberg.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICkkw0jYCkX2qhnLZxLPa4vEs97gRFM+F8zXKNdOY7Xn Codeberg\n";
+    home.file.".ssh/twoflower.pub".text = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCrvW1wIRC7BJ6Hp7NUlO4q4VUQPze2Gn+XstOOgL81PC/Rkkp2awx33suCkLvxdNL5YyXiw8N0JmFA4DsjWhFXnQuNAtMB01CICUVwexTxw8ZtmEOOcY5xwNaK/xfbl5+QCNgq0bEl3SBYmfnh2sNXHHMQNchPIjYZtLLzZ7QTDZrjOTmeqr0otmH6JK8oo/f/8G2/9NkY75GDcjwaPv6R4aH7nlO4hehLp58bYo3A/u7hcrvXz77h8On9mDyLu1u/LkH7rqBxjYfvNAqInCxT7BWb4YDci0Ho8c5NkRuHADzmkR6uViwCmJHWccu9yX5JddC9ySmsATuRlNBab9VD Personal\n";
+    home.file.".ssh/twoflower-borg.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAXXOzpXjC3xucybQo9nmQAkjOZuEJSqxVo1RdDst1zz Twoflower Borg Weatherwax\n";
     programs.ssh = {
       enable = true;
       # Default values will be removed in the future
       enableDefaultConfig = false;
       settings = {
-        # Pin a specific key for Codeberg, otherwise we get too many auth failures after trying all the keys in Bitwarden.
         "codeberg.org" = {
           IdentityFile = "~/.ssh/codeberg.pub";
+          IdentitiesOnly = true;
+        };
+        "twoflower.octo.cafe" = {
+          IdentityFile = "~/.ssh/twoflower.pub";
+          IdentitiesOnly = true;
+        };
+        "twoflower-borg" = {
+          HostName = "twoflower.octo.cafe";
+          User = "borg";
+          Port = 2222;
+          IdentityFile = "~/.ssh/twoflower-borg.pub";
           IdentitiesOnly = true;
         };
         "*" = {
