625d486d92a78c72e0cf7431472f089d36c9ebe9
Vojtěch Balák <vojtech@balak.me>
2026-09-25 11:37:49 +0200

    Fix an issue with fonts in KDE title bars on atuin

--- hosts/atuin/user.nix
+++ hosts/atuin/user.nix
@@ -59,6 +59,23 @@ in
   home.sessionPath = [ "$HOME/.cargo/bin" ];
   # For some reason, Nix programs can't find the CA bundle on Fedora 44
   home.sessionVariables.NIX_SSL_CERT_FILE = "/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem";
+  # The two-axis variable Noto Sans files from nixpkgs noto-fonts (NotoSans[wdth,wght].ttf and the
+  # Italic one) break Qt 6.11 font matching on Fedora 44: "Noto Sans" Regular/Italic silently fall
+  # back to Noto Sans Arabic line metrics, which makes Breeze title bars and Qt line spacing ~50 %
+  # too tall. Hide them so Fedora's static NotoSans-Regular.ttf is used (Fedora's own single-axis
+  # NotoSans[wght].ttf is fine). Re-check after qt6-qtbase / fontconfig / noto-fonts updates.
+  xdg.configFile."fontconfig/conf.d/60-reject-noto-vf.conf".text = ''
+    <?xml version="1.0"?>
+    <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+    <fontconfig>
+      <selectfont>
+        <rejectfont>
+          <glob>*/share/fonts/noto/NotoSans[*</glob>
+          <glob>*/share/fonts/noto/NotoSans-Italic[*</glob>
+        </rejectfont>
+      </selectfont>
+    </fontconfig>
+  '';
   userModules = {
     genericLinux.enable = true;
     plasma.enable = true;
